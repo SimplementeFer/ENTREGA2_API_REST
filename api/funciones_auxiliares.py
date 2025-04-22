72cf8c3338429f5cf1bbcc67c5c6534b131d5b56
SimplementeFer <120043366+SimplementeFer@users.noreply.github.com>
2025-04-22 10:50:37 +0200

    Update funciones_auxiliares.py

--- api/funciones_auxiliares.py
+++ api/funciones_auxiliares.py
@@ -2,7 +2,7 @@
 
 import html
 import bleach  
-import bcrypt 
+# import bcrypt 
 
 def sanitize_input(user_input):
     """
@@ -11,13 +11,13 @@ def sanitize_input(user_input):
     escaped_input = html.escape(user_input)  # Escapa caracteres especiales en HTML
     return bleach.clean(escaped_input)  # Limpia cualquier HTML no permitido
     
-def cipher_password(password):
+#def cipher_password(password):
   hashAndSalt = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(10));
-  return hashAndSalt
-def compare_password(password_hash,password):
-   if password_hash is None:
-      return False
-   try:
-      return bcrypt.checkpw(password,password_hash)
-   except:
-      return False
+  #return hashAndSalt
+#def compare_password(password_hash,password):
+   #if password_hash is None:
+      #return False
+   #try:
+      #return bcrypt.checkpw(password,password_hash)
+   #except:
+      #return False
